83dade73a57f984827a7d6ccc41534b63f210ed3
Zoe Anne Dyson <zadyson@gmail.com>
2018-02-22 10:21:33 +1100

    Update genotyphi.py (H58 lineages 1/2 & QRDR)
    Updated version of genotyphi.py that will further genotype lineages of genotype 4.3.1 (H58) as 4.3.1.1 (H58 lineage 1) and 4.3.1.2 (H58 lineage).  This update also includes additional code to call known quinolone-resistance determining region (QRDR) mutations in genes gyrA and parC.

--- genotyphi.py
+++ genotyphi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Input BAM (recommended) or VCF (if highly trusted SNP data) relative to Typhi CT18 (AL513382) and assign Typhi genotype codes.
+# Input BAM (recommended) or VCF (if highly trusted SNP data) relative to Typhi CT18 (AL513382) and assign Typhi genotype codes and detect QRDR mutations.
 #
 # Authors - Kat Holt (kholt@unimelb.edu.au), Zoe Dyson (zoe.dyson@unimelb.edu.au)
 #
@@ -9,7 +9,7 @@
 # Dependencies:
 #	 SAMtools (v1.2) and bcftools (v1.2) are required to genotype from BAMs.
 #
-# Last modified - Jun 7th, 2016
+# Last modified - Feb 1st, 2018
 #
 
 from argparse import (ArgumentParser, FileType)
@@ -47,24 +47,67 @@ def parse_args():
 						help='Location of folder containing bcftools installation if not standard/in path.')
 	return parser.parse_args()
 
-### SNP definitions
+### Genotype SNP definitions
 
 loci = [655112, 773487, 1804415, 1840727, 3640678, 270120, 102135, 316489, 4105384, 555826, 2360997, 4664137, 2166082,
 		30192, 4288272, 2737027, 1215983, 4132985, 146673, 2517324, 3920009, 3276735, 1173984, 2683312, 4013386,
 		4094437, 827036, 3476114, 4355243, 2723847, 4388609, 703762, 431216, 3095443, 316186, 1934711, 2811222, 3092900,
 		2723724, 3437570, 1780319, 1535365, 1792810, 3062270, 1799842, 432732, 3069182, 2732615, 3770391, 2269835,
 		4215341, 4602946, 3368641, 2245432, 3164162, 3923165, 1811809, 3729635, 3817752, 183033, 1615350, 2342045,
-		3996717, 2640029, 989024, 3806278, 1611156, 2348902]
+		3996717, 2640029, 989024, 3806278, 1611156, 2348902, 1193220, 3694947]
 snp_alleles = ['T', 'A', 'C', 'A', 'A', 'T', 'A', 'C', 'T', 'A', 'T', 'T', 'A', 'T', 'T', 'G', 'G', 'A', 'A', 'A', 'T',
 			   'A', 'T', 'A', 'A', 'A', 'A', 'T', 'C', 'A', 'C', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'C', 'A', 'T',
 			   'T', 'C', 'C', 'T', 'G', 'A', 'T', 'G', 'C', 'T', 'T', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'T', 'A', 'T',
-			   'A', 'T', 'A', 'A', 'T']
+			   'A', 'T', 'A', 'A', 'T', 'C', 'G']
 groups = ['0.1', '0.0.1', '0.0.2', '0.0.3', '0.1.1', '0.1.2', '0.1.3', '1', '1.1', '1.1.1', '1.1.2', '1.1.3', '1.1.4',
 		  '1.2', '1.2.1', '2', '2.0.1', '2.0.2', '2.1', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.1.5', '2.1.6', '2.1.7',
 		  '2.1.8', '2.1.9', '2.2', '2.2.1', '2.2.2', '2.2.3', '2.2.4', '2.3', '2.3.1', '2.3.2', '2.3.3', '2.3.4',
 		  '2.3.5', '2.4', '2.4.1', '2.5', '2.5.1', '3', '3.0.1', '3.0.2', '3.1', '3.1.1', '3.1.2', '3.2', '3.2.1',
 		  '3.2.2', '3.3', '3.3.1', '3.4', '3.5', '3.5.1', '3.5.2', '3.5.3', '3.5.4', '4', '4.1', '4.1.1', '4.2',
-		  '4.2.1', '4.2.2', '4.2.3', '4.3.1']
+		  '4.2.1', '4.2.2', '4.2.3', '4.3.1', '4.3.1.1', '4.3.1.2']
+
+### QRDR SNP definitions
+
+qrdr_loci = [2333762, 2333762, 2333750, 2333750, 2333751, 2333751, 3196469, 3196470, 3196458, 3196459] 
+qrdr_snp_alleles = ['A', 'T', 'A', 'C', 'A', 'T', 'T', 'A', 'C', 'T'] 
+qrdr_groups = [' gyrA-S83F', ' gyrA-S83Y', ' gyrA-D87V', ' gyrA-D87G', ' gyrA-D87Y', ' gyrA-D87N', ' parC-S80R', ' parC-S80I', ' parC-E84G', ' parC-E84K']
+
+
+# check if this SNP defines a QRDR group
+def checkQRDRSNP(vcf_line_split, this_qrdr_groups, qrdr_proportions, args):
+	qrdr_snp = int(vcf_line_split[1])
+	if qrdr_snp in qrdr_loci:
+		i = qrdr_loci.index(qrdr_snp)
+		if float(vcf_line_split[5]) > args.phred:
+			print vcf_line_split
+			m = re.search("DP4=(\d+),(\d+),(\d+),(\d+)", vcf_line_split[7])
+			if m != None:
+				alt_read_count = int(m.group(3)) + int(m.group(4))
+				total_read_count = alt_read_count + int(m.group(1)) + int(m.group(2))
+				if float(total_read_count) == 0:
+					qrdr_snp_proportion = float(-1)
+				else:
+					qrdr_snp_proportion = float(alt_read_count) / total_read_count
+			else:
+				if vcf_line_split[4] != '.': # if the ALT is not '.' i.e. if the alt is not same as ref
+					try:
+						ad = vcf_line_split[9].split(':')[1].split(',') # get the AD ratio
+						alt_read_count = int(ad[1])
+						total_read_count = int(ad[0]) + alt_read_count
+						qrdr_snp_proportion = float(alt_read_count) / total_read_count
+					except IndexError:
+						qrdr_snp_proportion = float(-1)
+
+				else:
+					qrdr_snp_proportion = float(-1)	# set unknowns to negative so that we know this is not a real proportion
+				
+			qrdr_snp_allele = vcf_line_split[4]
+			for position in xrange(0,10):
+				if (qrdr_snp == qrdr_loci[position]) and (qrdr_snp_allele == qrdr_snp_alleles[position]) and (qrdr_snp_proportion > args.min_prop):
+					this_qrdr_groups.append(qrdr_groups[position])  # Add QRDR SNP	
+
+	return (this_qrdr_groups)
+
 
 # check if this SNP defines a group
 def checkSNP(vcf_line_split, this_groups, proportions, args):
@@ -101,7 +144,6 @@ def checkSNP(vcf_line_split, this_groups, proportions, args):
 					proportions[groups[i]] = snp_proportion
 	return (this_groups, proportions)
 
-
 def checkSNPmulti(vcf_line_split, this_groups, args):
 	snp = int(vcf_line_split[1])
 	if snp in loci:
@@ -124,12 +166,20 @@ def parseGeno(this_groups, proportions):
 	primary = []
 	for group in this_groups:
 		level = len(group.split("."))
+		if level == 4:
+			subclades.append(group)
 		if level == 3:
 			subclades.append(group)
 		elif level == 2:
 			clades.append(group)
 		elif level == 1:
 			primary.append(group)
+
+	# fix 4.3.1/4.3.1.1/4.3.1.2 nesting
+	if ('4.3.1.1' in subclades) and ('4.3.1' in subclades):
+		subclades.remove('4.3.1')
+	if('4.3.1.2' in subclades) and ('4.3.1' in subclades):
+		subclades.remove('4.3.1')
 
 	# fix 2.3, 2.2 nesting
 	if ('2.2' in clades) and ('2.3' in clades):
@@ -278,7 +328,7 @@ def main():
 			vcfFiles = []
 			
 			# coordinates in zero-base, half-open for SAMtools compatible bed file
-			ordered_loci = list(loci)
+			ordered_loci = list(loci + qrdr_loci)
 			sorted(ordered_loci)
 			temp_bed_file = open(args.ref_id + '.bed', 'w')	 # create temporary bed file for SAMtools
 			for locus in ordered_loci:
@@ -315,7 +365,11 @@ def main():
 		if args.mode == 'bam':
 			output_file.write('\t'.join(
 				['File', 'Final_call', 'Final_call_support', 'Subclade', 'Clade', 'PrimaryClade', 'Support_Subclade',
-				 'Support_Clade', 'Support_PrimaryClade', 'Number of SNPs called\n']))
+				 'Support_Clade', 'Support_PrimaryClade', 'Number of SNPs called', 'QRDR mutations\n']))
+		elif args.mode == 'vcf':
+			output_file.write('\t'.join(
+				['File', 'Final_call', 'Final_call_support', 'Subclade', 'Clade', 'PrimaryClade', 'Support_Subclade',
+				 'Support_Clade', 'Support_PrimaryClade', 'QRDR mutations\n']))
 		else:
 			output_file.write('\t'.join(
 				['File', 'Final_call', 'Final_call_support', 'Subclade', 'Clade', 'PrimaryClade', 'Support_Subclade',
@@ -328,7 +382,9 @@ def main():
 			for vcf in args.vcf:
 				snp_count = 0
 				this_groups = []  # list of groups identified by defining SNPs
+				this_qrdr_groups = [] # list of QRDR SNPs found
 				proportions = {}  # proportion of reads supporting each defining SNP; key = group, value = proportion
+				qrdr_proportions = {}  # proportion of reads supporting each defining SNP; key = group, value = proportion
 
 				# read file
 				(file_name, ext) = os.path.splitext(vcf)
@@ -349,15 +405,17 @@ def main():
 							# parse this SNP line
 							any_ref_line = 1
 							(this_groups, proportions) = checkSNP(x, this_groups, proportions, args)
+							this_qrdr_groups = checkQRDRSNP(x, this_qrdr_groups, qrdr_proportions, args)
 
 				f.close()
 
+				print "qrdr groups".join(qrdr_groups)
 				if any_ref_line > 0:
 					info = parseGeno(this_groups, proportions)
 					if args.bam:
-						output_file.write(vcf + '\t' + info + '\t' + str(snp_count) + '\n')
+						output_file.write(vcf + '\t' + info + '\t' + str(snp_count) + '\t' + ','.join(this_qrdr_groups) + '\n')
 					else:
-						output_file.write(vcf + '\t' + info + '\n')
+						output_file.write(vcf + '\t' + info  + '\t' + ','.join(this_qrdr_groups) + '\n')
 				else:
 					output_file.write(
 						vcf + '\tNo SNPs encountered against expected reference. Wrong reference or no SNP calls?\n')
